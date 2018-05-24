14133836755e3607df829609813edc50daf66523
Sergey Fetisov <zerg903@mail.ru>
2018-05-24 10:45:08 +0300

    Правки по рекомендациям tslint

--- Sendbox/src/scripts/_script.tsx
+++ Sendbox/src/scripts/_script.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 class Hello extends React.Component {
-  render() {
+  public render() {
     return (
       <h2>Welcome to React!</h2>
     );
   }
 }
 
-ReactDOM.render(<Hello />,document.getElementById('app-root'));
+ReactDOM.render(<Hello />, document.getElementById('app-root'));
